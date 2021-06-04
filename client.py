c2fcb154aa386ff26b1b879b641c1a904f8a0e11
Sya Raihan Heggi <58820833+RaihanHeggi@users.noreply.github.com>
2021-06-04 21:20:10 +0700

    Update
    Sudah Diberi Komentar disetiap line untuk mengetahui fungsinya

--- client.py
+++ client.py
@@ -7,85 +7,129 @@ import struct
 
 # Fungsi Untuk Mengirimkan FIle Ke Server
 def upld(file_name):
+    # kita bersihkan command prompt agar rapi
+    clear()
+    # print untuk menandakan sedang berada di fungsi ana
     print("Uploading {}".format(file_name))
     try:
-        clear()
+        # buka file yang ingin dikirim dan baca perbaris
         with open(file_name, "rb") as handle:
+            # data yang dikirim merupakan baris data yang dibaca
             data = xmlrpclib.Binary(handle.read())
+            # file dikirim menggunakan fungsi upload yang ada di server
             server.file_upload(data, file_name)
     except Exception as e:
+        # jika terjadi eksepsi print eksepsinya
         print(e)
 
 
 # Fungsi untuk Mengunduh FIle Dari Server ke Client dan kemudian akan disimpan dengan nama hasilDownload.txt
 def dwnl(file_name):
+    # membersihkan command prompt
+    clear()
+    # print untuk  menandai proses download
     print("Downloading {}".format(file_name))
     try:
-        clear()
+        # buka file yang baru hasilDownload untuk menerima file yang dikirim server
         with open("hasilDownload.txt", "wb") as handle:
+            # tuliskan perbaris apa yang data yang diterima dari server
             handle.write(server.file_download(file_name).data)
+            # penulisan di tutup
             handle.close()
     except Exception as e:
+        # jika terjadi eksepsi print eksepsinya
         print(e)
 
 
 # getting server direktori file
 def listFile():
+    # print untuk menandai menu yang digunakan
     print("List File")
     try:
+        # panggil data yang diterima dari server yang berupa list nama barang di server
         data = server.list_file().data
+        # lakukan looping agar data bisa ditampilkan
         while True:
+            # bersihkan command prompt
             clear()
+            # print kalimat untuk mempercantik aplikasi
             print("Data Di Server \n")
+            # print data byte yang diterima dengan kita decode dulu ke format yang diketahui
             print(data.decode("utf-8"))
+            # input untuk menu
             prompt = input("Ingin Menutup Menu (Y/N) : ")
+            # jika Y maka kembali ke menu utama
             if prompt == "Y" or prompt == "y":
                 break
     except Exception as e:
+        # jika terjadi eksepsi maka print eksepsinya
         print(e)
 
 
+# fungsi yang digunakan untuk menampilkan menu dari aplikasi
 def menu():
+    # clear command prompt
     clear()
+    # print kata sambutan
     print("\nSelamat Datang FTP client.\n")
+    # lakukan looping terus hingga kondisi menghentikan aplikasi dan menu
     while True:
+        # bersihkan command prompt
         clear()
+        # print tampilan mempercantik menu
         print("++++++++++++++ MENU ++++++++++++++++")
+        # penjelasan perintah yang dapat digunakan
         print("\nGunakan Perintah Berikut Ini:")
+        # untuk melakukan upload bisa menggunakan UPLD dan file_pathnya
         print("UPLD file_path : Upload file")
+        # untuk melihat list dapat menggunakan LIST
         print("LIST           : List files")
+        # untuk melakukan download bisa menggunakan DWLD dan file_pathnya
         print("DWLD file_path : Download file")
+        # untuk menutup menggunakan QUIT
         print("QUIT           : Exit from Application")
+        # input menu
         prompt = input("\nSilahkan Masukkan Command : ")
-        if prompt[:4].upper() == "CONN":
-            conn()
-        elif prompt[:4].upper() == "UPLD":
+        # kita ambil 4 kalimat awal yang dimasukkan dan kita uppercase untuk perintah sesuai keperluan menu
+        if prompt[:4].upper() == "UPLD":
+            # Upload file dengan file_path yang berada pada kalimat 5 keatas
             upld(prompt[5:])
         elif prompt[:4].upper() == "LIST":
+            # buka fungsi listFile
             listFile()
         elif prompt[:4].upper() == "DWLD":
+            # buka fungsi download
             dwnl(prompt[5:])
         elif prompt[:4].upper() == "QUIT":
+            # Tutup Aplikasi
             print("Quit")
             break
         else:
+            # bila tidak ditemukan command print pemberitahuan
             clear()
             print("Command not recognised; please try again")
 
 
+# fungsionalitas utama
 def main():
+    # deklarasi global variabel yang dapat digunakan
     global server
     global clear
     global f
+    # buat try and except mencegah eksepsi
     try:
+        # buat fungsi untuk membersihkan command prompt windows dengan cls
         clear = lambda: os.system("cls")
         # Connect to Server
         server = xmlrpclib.ServerProxy("http://127.0.0.1:8000/")
         # Open Main Menu
         menu()
+    # jika socket connection error
     except socket.error as e:
+        # print pemberitahuan
         print("Socket Error")
         print(e)
+    # jika terjadi eksepsi lainnya
     except Exception as e:
         print(e)
 
--- server.py
+++ server.py
@@ -3,61 +3,101 @@ import xmlrpc.client
 from sys import argv
 import os
 
-
+# Fungsi yang digunakan untuk menerima file yang dikirimkan atau bila dilihat dari client melakukan proses upload
 def receive_file(filedata, filename):
+    # kita buat try dan except untuk mencegah jika terjadi eksepsi
     try:
+        # pertama kita buka fileUpload di server yang menerima pengiriman dari client
         with open("fileUpload.txt", "wb") as handle:
+            # filedata diterima dengan nama variabel json
             json = filedata.data
+            # kemudian fileUpload.txt diupdate line data yang digunakan
             handle.write(json)
+            # kita return True untuk mengakhiri proses
             return True
     except Exception as e:
+        # print eksepsi yang terjadi untuk mengetahui kesalahan yang terjadi
         print(e)
 
 
+# Fungsi yang digunakan untuk mengirimkan file dari server atau bila dilihat dari client melakukan proses download
 def sendFile(fileDownload):
+    # kita buat try dan except untuk mencegah jika terjadi eksepsi
     try:
+        # kita baca file yang ingin kita download yang berada di server
         with open(fileDownload, "rb") as handle:
+            # kita akan mengirimkan setiap apa yang dibaca pada file tersebut
             return xmlrpc.client.Binary(handle.read())
+            # kemudian pembacaan kita tutup
             handle.close()
     except Exception as e:
+        # print eksepsi yang terjadi untuk mengetahui kesalahan yang terjadi
         print(e)
 
 
+# Fungsi untuk menampilkan list data yang berada di server
 def listFile():
+    # kita buat try dan except untuk mencegah jika terjadi eksepsi
     try:
+        # pertama kita get data file yang berada di direktori menggunakan os.listdir()
+        arr = os.listdir()
+        # kemudian kita buat file yang menuliskan data byte yang dibaca sebelumnya agar bisa dikirim
+        with open("listDirektori.txt", "w+") as f:
+            # setiap item yang berada di arr nanti akan di tuliskan ke file direktori tersebut
+            for item in arr:
+                # lakukan write setiap item
+                f.write("%s\n" % item)
+            # kita tutup bila sudah di tulis
+            f.close
+        # selanjutnya kita buka file List Direktori tersebut untuk mengirimkan data Direktori di server
         with open("listDirektori.txt", "rb") as handle:
+            # Lakukan Pengiriman data tersebut perbaris data yang ada di file listdirektori
             return xmlrpc.client.Binary(handle.read())
+            # kita tutup proses pengiriman dan pembacaan
             handle.close()
     except Exception as e:
+        # print eksepsi yang terjadi untuk mengetahui kesalahan yang terjadi
         print(e)
 
 
 def main():
+    # kita buat try untuk mencegah eksepsi ketika menjalankan aplikasi
     try:
+        # cek jika argument yang dimasukkan di command prompt kurang dari 2 maka raise Index Error
         if len(argv) != 2:
             raise IndexError()
+        # catat nomor port pada kalimat kedua yang dimasukan pada command prompt
         port_num = int(argv[1])
+    # jika terjadi indexerror maka lakukan tahapan print cara menjalankan
     except IndexError:
         print("Gunakan Perintah> python server.py 'port'")
+        # tutup koneksi server tidak dinyalakan
         exit()
 
+    # buat try dan except ketika melakukan koneksi ke jaringan
     try:
+        # server menggunakan jaringan localhost yang dapat diganti dengan port sesuai keperluan dari user
         server = SimpleXMLRPCServer(("127.0.0.1", port_num), allow_none=True)
+        # print koneksi dibangun pada port berapa
         print("Listening On Port ", port_num)
-        arr = os.listdir()
-        with open("listDirektori.txt", "w+") as f:
-            for item in arr:
-                f.write("%s\n" % item)
-            f.close
+    # jika terjadi eksepsi
     except Exception as e:
+        # print eksepsi apa yang terjadi
         print(e)
+        # print pesan gagal
         print("Gagal menggunakan jaringan")
+        # tutup koneksi
         exit()
 
+    # proses registrasi fungsi agar dapat digunakan pada sisi client
     server.register_introspection_functions()
+    # receive file akan dikenali client sebagai fungsi upload
     server.register_function(receive_file, "file_upload")
+    # receive file akan dikenali client sebagai fungsi download
     server.register_function(sendFile, "file_download")
+    # receive file akan dikenali client sebagai fungsi list file
     server.register_function(listFile, "list_file")
+    # server dinyalakan selamanya hingga ada interupsi untuk mematikan
     server.serve_forever()
 
 
